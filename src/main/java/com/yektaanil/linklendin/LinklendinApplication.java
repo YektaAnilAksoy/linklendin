896dbd92036991746ff89f703fa31b70819f6ec7
YektaAnilAksoy <yektaanilaksoy@hotmail.com.tr>
2021-10-14 23:46:31 +0300

    Initial commit

---
+++ src/main/java/com/yektaanil/linklendin/LinklendinApplication.java
@@ -0,0 +1,13 @@
+package com.yektaanil.linklendin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklendinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinklendinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yektaanil/linklendin/LinklendinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yektaanil.linklendin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklendinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
